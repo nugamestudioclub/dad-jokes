93bab21b14cef366e37746b36562503bec21b0b2
samuel vayens <svayens@yahoo.com>
2024-01-27 15:33:29 -0500

    camera can now be controled by mouse movement

--- Assets/Scripts/dadCamFollowsDad.cs
+++ Assets/Scripts/dadCamFollowsDad.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dadMovementScript : MonoBehaviour
+public class moveDadCam : MonoBehaviour
 {
 
-    public GameObject dad;
+    public Transform cameraPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        dad = GameObject.Find("Cube");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = cameraPosition.position;
     }
 }

44f79ee29b9c71872da6ff275901f0289377d684
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2024-01-28 16:41:47 -0500

    added baby and ambient SFX

---
+++ Assets/Scripts/Soundset.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public struct SoundEffect {
+	[field: SerializeField]
+	public string Name { get; private set; }
+
+	[field: SerializeField]
+	public AudioClip AudioClip { get; private set; }
+}
+
+[CreateAssetMenu(fileName = "Soundset", menuName = "ScriptableObjects/Soundset")]
+public class Soundset : ScriptableObject {
+	public SoundEffect BabyNoise1;
+	public SoundEffect BabyNoise2;
+	public SoundEffect BabyNoise3;
+	public SoundEffect BabyNoise4;
+	public SoundEffect BabyLaugh1;
+	public SoundEffect BabyLaugh2;
+}
